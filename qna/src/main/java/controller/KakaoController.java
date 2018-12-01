2ac43c10d75140c3ac8204a06831d1fdf08a291a
minsikmoon <msmoon.business@gmail.com>
2018-12-01 23:10:04 +0900

    json simple은 double quotation 즉 쌍따옴표 해석을 못함. jackson으로 갈아타야할듯 : 여기서 종료

--- qna/src/main/java/controller/KakaoController.java
+++ qna/src/main/java/controller/KakaoController.java
@@ -1,12 +1,18 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import dao.KakaoDao;
@@ -33,15 +39,54 @@ public class KakaoController {
 		//1. jsonarray만들기
 		JSONObject responseItem = new JSONObject();
 		JSONArray buttonItem = new JSONArray();
-
 		//make buttons
 		buttonItem.add("질문하기");
 		buttonItem.add("답변보기");
-
 		//make response json
 		responseItem.put("type", "buttons");
 		responseItem.put("buttons", buttonItem);
 	
+		return responseItem.toJSONString();
+	}
+	//message
+	@RequestMapping(value = "/message", method = RequestMethod.POST, produces = "application/json;charset=utf8")
+	public @ResponseBody String message(@RequestBody String request){
+		JSONParser reqParser = new JSONParser();
+		Object reqObj;
+		JSONArray reqArray;
+		String resString = "";
+
+		String userKey, type, content; //request
+		JSONObject message, keyboard; //response
+
+		JSONObject responseItem = new JSONObject();
+		keyboard = new JSONObject();
+		message = new JSONObject();
+
+		// parse request 
+		try {
+			System.out.println(request);
+			reqObj = reqParser.parse(request);
+			reqArray = (JSONArray)reqObj;
+			System.out.println("hihihihi" + reqArray.size());
+			//userKey = (String)reqObj.get("\"user_key\"");
+			//content = (String)reqObj.get("content");
+			//type = (String)reqObj.get("type");
+			//System.out.println(userKey + content + type);
+			// if(content.equals("질문하기")){
+			// 	message.put("text", "질문을 입력해주세요.");
+			// 	keyboard.put("type", "text");
+			// }else{
+			// 	message.put("text", "답변을 입력해주세요.");
+			// 	keyboard.put("type", "text");
+			// }
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		responseItem.put("message", message);
+		responseItem.put("keyboard", keyboard);
+
+		//make response 
 		return responseItem.toJSONString();
 	}
 }
