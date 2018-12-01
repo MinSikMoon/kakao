96c326403e85059c5227382cad8b17709bd2bfbf
minsikmoon <msmoon.business@gmail.com>
2018-12-01 20:49:54 +0900

    keyboard 및 한글깨짐 해결 : pom에 java compiler가 두번 들어감

--- qna/src/main/java/controller/KakaoController.java
+++ qna/src/main/java/controller/KakaoController.java
@@ -2,6 +2,8 @@ package controller;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,29 @@ public class KakaoController {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	@RequestMapping(value = "/Test")
-	public @ResponseBody void mybatisTest() {
+	@RequestMapping(value = "/test")
+	public @ResponseBody void test() {
 		KakaoDao mdao = sqlSession.getMapper(KakaoDao.class);
 		for(KakaoVo vo : mdao.selectAll()){
 			System.out.println(vo.getTitle());
 		}
+	}
+
+	//keyboard
+	@RequestMapping(value = "/keyboard", produces="application/json;charset=utf8")
+	public @ResponseBody String keyboard() {
+		//1. jsonarray만들기
+		JSONObject responseItem = new JSONObject();
+		JSONArray buttonItem = new JSONArray();
+
+		//make buttons
+		buttonItem.add("질문하기");
+		buttonItem.add("답변보기");
+
+		//make response json
+		responseItem.put("type", "buttons");
+		responseItem.put("buttons", buttonItem);
+	
+		return responseItem.toJSONString();
 	}
 }
